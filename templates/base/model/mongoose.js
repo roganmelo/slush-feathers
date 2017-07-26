304faf791c9e439ac385a00c7d7f7d0f6dec27e3
RoganMelo <roganmelo@gmail.com>
2017-07-26 19:15:38 -0300

    improve some code patterns on mongoose model

--- templates/base/model/mongoose.js
+++ templates/base/model/mongoose.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   const mongooseClient = app.get('mongooseClient');
   const <%= camelName %> = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    text:      { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
